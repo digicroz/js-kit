bb241f757e8c2adb0ca454421c5435c4db216ca6
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2026-01-19 16:44:03 +0530

    added standerd response

--- src/std-response/index.ts
+++ src/std-response/index.ts
@@ -3,15 +3,29 @@ export type StdSuccess<T> = {
   result: T;
 };
 
-export type StdError<E extends string | number = string> = {
+export type StdError<E extends string | number> = {
   status: "error";
   error: {
     code: E;
     message?: string;
-
   };
 };
 
-export type StdResponse<T, E extends string | number = string> =
+export type StdResponse<T, E extends string | number> =
   | StdSuccess<T>
   | StdError<E>;
+
+export const stdResponse = Object.freeze({
+  success: <T>(result: T): StdSuccess<T> => ({
+    status: "success",
+    result,
+  }),
+
+  error: <E extends string | number>(
+    code: E,
+    message?: string
+  ): StdError<E> => ({
+    status: "error",
+    error: { code, message },
+  }),
+});

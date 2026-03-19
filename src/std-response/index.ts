bf88d8e06fa5bebb6e6035d9875d1a6316110b9c
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2026-03-20 00:30:44 +0530

    added helper types

--- src/std-response/index.ts
+++ src/std-response/index.ts
@@ -31,3 +31,20 @@ export const stdResponse = Object.freeze({
     error: { code, message, details },
   }),
 });
+
+// helper types 
+
+export type StdExtractSuccess<T> = Extract<T, { status: "success" }>;
+export type StdExtractError<T> = Extract<T, { status: "error" }>;
+export type StdExtractResult<T> =
+  Extract<T, { status: "success" }> extends { result: infer D }
+    ? D
+    : never;
+
+export type StdExtractErrorCode<T> =
+  Extract<T, { status: "error" }> extends {
+    error: { code: infer C };
+  }
+    ? C
+    : never;
+

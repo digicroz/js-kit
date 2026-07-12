dfe546cd9fd87539a84f132543dfd78c7ba5567f
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2026-07-12 07:26:27 +0530

    added defineAllValues

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,18 @@
 export type Prettify<T> = {
-    [K in keyof T]: T[K];
-    // eslint-disable-next-line @typescript-eslint/ban-types
+  [K in keyof T]: T[K];
+  // eslint-disable-next-line @typescript-eslint/ban-types
 } & {};
+
+export type AllowedKeys<T> = keyof T;
+
+type ExactUnionArray<Union, Arr extends readonly Union[]> =
+  Exclude<Union, Arr[number]> extends never
+    ? Exclude<Arr[number], Union> extends never
+      ? Arr
+      : never
+    : never;
+
+export function defineAllValues<Union>() {
+  return <Arr extends readonly Union[]>(values: ExactUnionArray<Union, Arr>) =>
+    values;
+}
